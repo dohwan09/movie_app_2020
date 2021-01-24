cc47e76472ed7cc9d46545d9ee953f1f6904c699
dohwan09 <dhleekr12@gmail.com>
2021-01-24 21:50:03 +0900

    Updates

---
+++ precache-manifest.0376e9c1a50d21625cdaa4cc736e668e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f325c6a1d929366dc7f300c6908e48e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "09e5c1bf936ca260886c",
+    "url": "/movie_app_2020/static/css/main.94174939.chunk.css"
+  },
+  {
+    "revision": "e2db8dbeff0daf97e999",
+    "url": "/movie_app_2020/static/js/2.3c0d2268.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.3c0d2268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e5c1bf936ca260886c",
+    "url": "/movie_app_2020/static/js/main.32fa4e95.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
